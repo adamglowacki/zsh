1951a889d4a8034a872b4ad5922a027f952f71ea
Adam Głowacki <adam@sybet.pl>
2019-12-18 08:04:41 +0100

    pushall (added --all)

--- git_push_all.zsh
+++ git_push_all.zsh
@@ -1,1 +1,1 @@
-alias pushall='for i in `git remote`; do echo ">> Pushing to" $i "..."; git push $i; done;'
+alias pushall='for i in `git remote`; do echo ">> Pushing to" $i "..."; git push --all $i; done;'
