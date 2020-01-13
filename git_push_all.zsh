f9e1cf1d40b56e031a94d8f247fbc1395307b31f
Adam Głowacki <adam@sybet.pl>
2020-01-13 14:33:58 +0100

    pushall: print green/red result for each remote

--- git_push_all.zsh
+++ git_push_all.zsh
@@ -1,1 +1,1 @@
-alias pushall='for i in `git remote`; do echo ">> Pushing to" $i "..."; git push --all $i; done;'
+alias pushall='for i in `git remote`; do echo ">> Pushing to" $i "..."; git push --all $i && echo "\e[32mOK\e[0m" || echo "\e[31mFAILED\e[0m"; done;'
